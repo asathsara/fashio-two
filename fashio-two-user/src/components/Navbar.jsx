2d3c7d6d195c73c5a6a6ff8c7555947b6716adaa
A S <65203960+asathsara@users.noreply.github.com>
2024-12-23 20:15:39 +0530

    Added google fonts & working on navbar

---
+++ fashio-two-user/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+const Navbar = () => {
+  return (
+    <nav class="navbar">
+      <div className="flex bg-counterBlue w-full">
+      <div className="flex-1 e p-4 text-center font-pacifico">Fashio</div>
+      <div className="flex-[3] p-4 text-center">3</div>
+      <div className="flex-1  p-4 text-center">1</div>
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar
