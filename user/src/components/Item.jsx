fae474cbf89738846761e143803472bf96d81a20
A S <65203960+asathsara@users.noreply.github.com>
2025-01-12 22:29:25 +0530

    User: item UI improved

--- user/src/components/Item.jsx
+++ user/src/components/Item.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Item = ({ className, item }) => {
   return (
-    <div className={`${className} rounded-md border-1 border-x-gray-100 w-96`}>
+    <div className={`${className} rounded-md border-1 border-x-gray-100 md:w-64 mr-10 mt-10 w-56`}>
       <img
-        className="rounded-t-md h-64 w-96"
+        className="rounded-t-md md:h-72 h-64 w-full object-cover"
         src={import.meta.env.VITE_API_UPLOAD_IMAGES_URL + item.urls[0]}
         alt={item.name}
       />
-      <div className="flex flex-col">
-        <p className="text-lg text-gray-500 mt-8">{item.name}</p>
-        <h2 className="text-sm font-bold mt-2 mb-6">{item.price}</h2>
+      <div className="flex flex-col w-full items-center font-poppins">
+        <p className="text-lg text-darkGray mt-8">{item.name}</p>
+        <h2 className="text-md font-bold mt-2 mb-6">{`Rs. ${(Math.round(item.price * 100) / 100).toFixed(2)}`}</h2>
       </div>
     </div>
   );
--- user/src/components/ItemCategory.jsx
+++ user/src/components/ItemCategory.jsx
@@ -3,16 +3,16 @@ import Item from "./Item";
 
 const ItemCategery = ({ className, categoryName, items }) => {
   return (
-    <div className={`${className} flex flex-col`}>
-      <div className="flex flex-row">
-        <p className="font-bold text-lg">{`${categoryName}'s`}</p>
-        <p className="ml-4 text-lg font-bold  stroke-current">
-          Collection
-        </p>
+    <div className={`${className} flex flex-col font-poppins  mx-1/10`}>
+      <div className="flex flex-row mt-10">
+        <p className="font-bold md:text-5xl text-4xl">{`${categoryName}'s`}</p>
+        <p className="ml-4 md:text-5xl text-4xl font-bold text-outline">Collection</p>
       </div>
-      {items.map((item) => {
-        return <Item key={item._id} item={item} />;
-      })}
+      <div className="flex flex-row flex-wrap">
+        {items.map((item) => {
+          return <Item key={item._id} item={item} />;
+        })}
+      </div>
     </div>
   );
 };
