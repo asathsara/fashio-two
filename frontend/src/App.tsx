4471b2c8469be64279022c2928874da9aaa1207c
A S <65203960+asathsara@users.noreply.github.com>
2025-11-11 19:15:47 +0530

    refactor: update Toaster component and add custom styles for toast notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { AuthProvider } from "@/contexts/auth/AuthProvider";
 import { AppRoutes } from "@/AppRoutes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { Toaster } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
 
 
 const queryClient = new QueryClient()
@@ -14,7 +14,7 @@ function App() {
       <AuthProvider>
         <BrowserRouter>
           <AppRoutes />
-          <Toaster position="top-right" richColors />
+          <Toaster position="top-right" />
           <ReactQueryDevtools initialIsOpen={false} />
         </BrowserRouter>
       </AuthProvider>
