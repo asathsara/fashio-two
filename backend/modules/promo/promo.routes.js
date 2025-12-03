c588f667c90ca65df331361db65bba206034ef6c
A S <65203960+asathsara@users.noreply.github.com>
2025-12-03 15:04:27 +0530

    feat: add authentication middleware to promo routes for enhanced security

--- backend/modules/promo/promo.routes.js
+++ backend/modules/promo/promo.routes.js
@@ -1,18 +1,21 @@
 import { Router } from "express";
 import promoController from './promo.controller.js';
 
+import authMiddleware from '../../middleware/auth.js';
+const { protect, admin } = authMiddleware;
+
 const router = Router();
 
 // Create
-router.post("/add", (req, res) => promoController.addPromo(req, res));
+router.post("/add", protect, admin, (req, res) => promoController.addPromo(req, res));
 
 // Update
-router.put("/:id", (req, res) => promoController.updatePromo(req, res));
+router.put("/:id", protect, admin, (req, res) => promoController.updatePromo(req, res));
 
 // Read
 router.get("/", (req, res) => promoController.getAllPromos(req, res));
 
 // Delete
-router.delete("/:id", (req, res) => promoController.deletePromo(req, res));
+router.delete("/:id", protect, admin, (req, res) => promoController.deletePromo(req, res));
 
 export default router;
